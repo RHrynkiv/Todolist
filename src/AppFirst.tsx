7d435235cd7407a2b92452a4e62764e511e5c9d9
ROMA <rgrinkiv030924@gmail.com>
2022-04-13 21:41:57 +0300

    I hope it`s last commit

---
+++ src/AppFirst.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Navigate, Route, Routes} from "react-router-dom";
+import App from "./AppWithReducers";
+import {Login} from "./features/Login";
+
+function AppFirst(){
+    return(
+        <Routes>
+            <Route path="/" element={<App/>}/>
+            <Route path="login" element={<Login/>}/>
+            <Route path="/404" element={<h1>404. Page not found</h1>}/>
+            <Route path="*" element={<Navigate to="/404"/>}/>
+        </Routes>
+    )
+}
+export default AppFirst
